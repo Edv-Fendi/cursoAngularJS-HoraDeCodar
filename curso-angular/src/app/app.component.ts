97806dab4cdfc7106c5f57cf2f043cc4271ddae9
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2022-11-23 09:23:42 -0300

    components

--- curso-angular/src/app/app.component.ts
+++ curso-angular/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Edvaldo';
+
+  userData = {
+    email: "edvaldo@gmail.com",
+    role: 'admin',
+  }
+
   title = 'curso-angular';
 }
